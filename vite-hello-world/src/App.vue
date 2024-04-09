53e7de6867d499ccd6e816bcb2c8f7c821d769be
gab <corbanigabriele@gmail.com>
2024-04-09 14:58:54 +0200

    ADD: Esercizio

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      AppTitle: 'La mia prima app con Vite!',
+      cliccatore: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="title">
+      {{ AppTitle }}
+
+    </h1>
+    <div>
+      <button @click="cliccatore++">
+        clicca
+      </button>
+      <span class="spazio"> {{ cliccatore }}</span>
+    </div>
+  </div>
+</template>
+
+<style>
+  .spazio {
+    margin: 10px
+  }
+</style>
